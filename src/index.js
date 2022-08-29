64f726eb7b34f4d0bebc938c75c6666c85a8aba8
Kurtis Houser <houserkurtis@gmail.com>
2022-08-29 10:57:09 -0700

    Add clientOptions for prod and dev

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'https://apollo-gql-catstronauts.herokuapp.com/', // change to YOUR own production server
-  cache: new InMemoryCache(),
-  name: 'web',
-  version: '1.0',
-});
+const clientOptions =
+  process.env.NODE_ENV === 'production'
+    ? {
+        uri: 'https://apollo-gql-catstronauts.herokuapp.com/', // change to YOUR own production server
+        cache: new InMemoryCache(),
+        name: 'web',
+        version: '1.0',
+      }
+    : {
+        uri: 'http://localhost:4000',
+        cache: new InMemoryCache(),
+      };
+
+const client = new ApolloClient(clientOptions);
 
 ReactDOM.render(
   <ApolloProvider client={client}>
